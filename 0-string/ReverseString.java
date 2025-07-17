0c8274e4513d3b7cb6fd430ef31b768c6b248621
Xavier Chopin <xavierchopin.ca@gmail.com>
2025-07-17 09:43:34 -0700

    Create ReverseString.java

---
+++ 0-string/ReverseString.java
@@ -0,0 +1,15 @@
+class ReverseString {
+      public void reverseString(char[] s) {
+        int l = 0;
+        int r = s.length - 1;
+
+        while (l < r) {
+            char temp = s[l];
+            s[l] = s[r];
+            s[r] = temp;
+
+            l++;
+            r--;
+        }
+    }
+}
